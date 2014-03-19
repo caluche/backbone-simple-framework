fc19293dd6938a2ee8b572d852b2af93155cd301
b-ma <benjamin.matuszewski@gmail.com>
2014-03-19 22:47:10 +0100

    improve comments

--- public/js/main.js
+++ public/js/main.js
@@ -8,13 +8,26 @@ define(
         /**
          *  @TODO
          *      create a scheme of architecture
-         *      write docs / cookbook
-         *      unit test
+         *      write docs
+         *      write a cookbook (to test api as well as a list of how to)
+         *      unit tests
          */
 
+        /**
+         *  the following still needs to be implemented:
+         *      - a service wrapping require.js to load modules from `config.path`
+         *      - an asset loader acting as an interface between third party library and framework
+         *      - a layout object
+         *      - a region object also able to execute a transition between 2 views
+         *      - a view factory able to create and delete view objects (used in controllers)
+         *      - ...
+         */
+
+
+
         //  get environment globals from requirejs
-        //  @WARNING is considered as a semi-private API could break at any time
-        //  cf. https://groups.google.com/forum/#!topic/requirejs/4KXx7AoTDUs (check `context` option)
+        //  @WARNING is considered as a semi-private API, could break at any time
+        //  cf. https://groups.google.com/forum/#!topic/requirejs/4KXx7AoTDUs (@todo - check `context` option)
         var env = requirejs.s.contexts._.config.globals;
 
         // hack Backbone.History
@@ -48,39 +61,53 @@ define(
         };
         // */
 
-        //  ---------------------------------------
-        //  @TODO    create a constructor to allow it being used in app
-        //  @API     rename to `com`
-        //  keep it external
+        /**
+         *  FRAMEWORK GLOBAL COMMUNICATION OBJECT
+         *
+         *  basically a Backbone.Events with a pubsub interface (alias method signatures)
+         *  (looks more meaningfull for this kind of functionnality)
+         *  allow decoupling as well as publishing the internal state of the framework for plugins
+         *
+         *  @TODO    create a constructor to allow it being used somewhere else in the application
+         */
         var com = _.extend({}, Backbone.Events, {
+            // alias `trigger`
             publish: function() {
                 var args = Array.prototype.slice.call(arguments, 0);
                 this.trigger.apply(this, args);
             },
 
+            // alias `on`
             subscribe: function() {
                 var args = Array.prototype.slice.call(arguments, 0);
                 this.on.apply(this, args);
             },
 
+            // alias `off`
             unsubscribe: function() {
                 var args = Array.prototype.slice.call(arguments, 0);
                 this.off.apply(this, args)
             },
 
-            // kind of log system
-            trace: function() {},
+            // added methods
+            // --------------------------------------------------------
+            // kind of log system should be able to :
+            //      - trace a route
+            //      - a specific object of the framework (a specific namespace)
+            //      - make a list of existing channels with their related subscriptions
+            //      - ...
+            trace: function(key) {},
+            // remove all subscriptions of an object at once
+            removeHandler: function(obj) {}
         });
-        // @TODO - get a log system
 
         /**
-         *  Framework boostrapper
+         *  bootstrap - init and configure core parts of the framework
          */
         var GG = {
             initialize: function(config, env) {
                 this.config = config;
                 this.env = env;
-                // @NOTE    maybe use a Backbone.Collection (needs use cases)
 
                 // install global services/plugin that could used in controllers
                 // allow plugin developpement and code reuse without altering the framework
@@ -88,9 +115,11 @@ define(
                 //      maybe a kind of window monitoring could be registered as a service or plugin
                 //      could be used to install proxies with third party services (asset loader, etc...)
                 // @NOTE    maybe redondant with `this.plugins`
-                // @IMPORTANT the asset loader must be a service/plugin
-                this.plugins = {};
-                // this.services = {};
+                //          maybe use a Backbone.Collection (needs use cases)
+                // @IMPORTANT   assetLoader, AutoLoader must be services
+                //              make a choice between `plugins` and `services`
+                //              (maybe `services` should be used for plugins needed by the framework and `plugins` elsewhere)
+                this.services = this.plugins = {};
 
                 // create core objects
                 this.initRouter();
@@ -106,8 +135,10 @@ define(
                 }
             },
 
-            // get a ref to com object (usefull for plugins API)
-            com: com,
+            //  com : maybe keeping it external to GG object is more meaningfull
+            //        but maybe harder to trace/debug
+            //  get a reference to the `com` object (usefull for plugins construction, should not be used elsewhere)
+            //  com: com,
 
             initRouter: function() {
                 this.router = new Router({
@@ -116,7 +147,7 @@ define(
             },
 
             initDispatcher: function() {
-                // @MAYBE this should be a collection of smaller models - ??
+                // @NOTE - maybe should be a collection of smaller models - ??
                 this.dispatcher = new Dispatcher({
                     states: this.config.states,
                     paths: this.config.paths,
@@ -125,7 +156,7 @@ define(
                 });
             },
 
-            //  map states to Bacbone.Router compliant routes
+            //  map `config.states` to Backbone.Router compliant routes
             getRoutes: function(config) {
                 var routes = {},
                     states = config.states;
@@ -149,17 +180,18 @@ define(
 
             },
 
-            //  @TODO   allow switch between service and plugin installation
-            //  or maybe it's just the same thing finally
+            //  method to install plugins/services to the framework
             //
             //  @param pluginName <string> id of the plugin to get it back in controllers
             //  @param pluginCtor <object>
             //          constructor of the plugin
-            //          the current instance of the framework is passed as argument
+            //          the current instance of the framework and coms is passed as argument
+            //
+            //  @TODO (maybe)   allow switch between service and plugin installation
             install: function(pluginName, pluginCtor) {
-                // each plugins receive access to the whole
-                // framework, (communication are available through GG.com)
-                var plugin = new pluginCtor(this);
+                // each plugins receive access to the whole framework and coms
+                // var plugin = new pluginCtor(this);
+                var plugin = pluginCtor(this, com); // maybe is better
                 // add to collection
                 // services and plugins should be singletons
                 if (!this.plugins[pluginName]) {
@@ -168,8 +200,8 @@ define(
             }
         };
 
-        //  could be added to underscore: cf. _.mixin()
-        //  @NOTE   if in underscore method names should be configurable
+        //  could/should be added to underscore: cf. _.mixin()
+        //  @NOTE   if in underscore, method names should be configurable
         GG.utils = {
             /**
              *  @param obj <object>     object to test
@@ -179,7 +211,7 @@ define(
             ensureApi: function(obj, methods) {},
         }
 
-        // could be a Backbone.View with `window` as `$el` to use Backbone's jQuery instance
+        // could be a Backbone.View with `window` or `document` as `$el` to use Backbone's jQuery instance
         var UILocker = function() {
             //  subscribe to `app:lock` and `app:unlock` channels
             this.subscribe('ui:lock', this.lock, this);
@@ -187,30 +219,30 @@ define(
         }
 
         UILocker.prototype = {
-            // lock interactions from the `window`
-            // app should be locked from Disptacher
+            // lock user interactions
+            // app should be locked from the Disptacher
             lock: function() {},
-            // should be unlocked from the Layout (once all regions finished transitions)
+            // and unlocked from the Layout (once all Regions finished their transitions (cf. `_.times`))
+            // Layout should subscribe to all channels from Dispatcher and Loader
             unlock: function() {},
         }
-
-        // once GG is called config.paths allow to map controller names with file system
 
         /**
          *  ROUTER
          *
          *  cf. https://github.com/mlmorg/backbone.canal : allow to have { name: param } mapping
-         *  maybe first just use it and then rewrite the usefull parts
-         *  other parts could also be used as inspiration for the dispatcher
+         *  maybe first just wrap it and then rewrite the usefull parts
+         *  some other parts could also be used as inspiration for the dispatcher
          *
-         *  should be able to map arguments their names (and then merge with defaults params)
+         *  should be able to map arguments with their names (and then merge with defaults params)
          *
          *  -------------------------------------------
          *
-         *  @TODO   should have the ability to create urls from route and params
-         *              - will probably not work with regexps in routes
-         *          add a catch all route (or do it in config finally)
-         *          create a real request object (Backbone Model)
+         *  @TODO   should be able to create urls from a routeId and given parameters
+         *              - will not work with regexps in routes
+         *              - that's why it's not in backbone, see constraints/benefits (cf. SEO, url based states, ...)
+         *          add a catch all route (or do it in config)
+         *          maybe create a real request object, extending `Backbone.Model` (see if it's usefull)
          */
         var Router = Backbone.Router.extend({
             initialize: function() {
@@ -219,6 +251,7 @@ define(
 
             // count concurrent routes
             counter: 0,
+
             /**
              *  forward the request to the dispatcher
              *  @EVENT global `route:change`
@@ -231,7 +264,7 @@ define(
                 var that = this;
                 this.counter++;
 
-                // this allow to know if we are in multiple route configuration or not
+                // this allow to know if we are currently in a multiple route configuration or not
                 // @TODO    need to be tested cross-browser, not sure it's really reliable
                 _.defer(function() {
                     var options = {
@@ -252,18 +285,24 @@ define(
          *  -------------------------------------------------------
          *  find the state and the according controller
          *  destroy the previous controller if different
-         *  instanciate the controller and call its action method
+         *  instanciate the controller and calls its `action` method
          *
          *  EVENTS: dispatch events before and after dispatch to allow easy plug-in
          *
          *  the dispatcher (and layout) use `require` to load and use modules
+         *  @NOTE - it should use a service wrapping `require.js` to load modules
+         *  (`config.paths` allow to map `controllers` with the file system)
+         *
          *  http://stackoverflow.com/questions/9507606/when-to-use-require-and-when-to-use-define
-         *  @NOTE - is this can create problems with r.js ?
+         *  @NOTE - can create problems with r.js ?
          */
         var Dispatcher = function(options) {
             this.states = options.states;
+            //  map the filesystem with namespaces (basically allow factories to work)
             this.paths = options.paths;
-            this.app = options.app; // store references to framework plugins and services
+            //  store references to framework plugins and services
+            //  allow controllers to use services
+            this.app = options.app;
 
             this.previousCommand = {};
             this.previousController;
@@ -274,9 +313,10 @@ define(
         }
 
         _.extend(Dispatcher.prototype, Backbone.Events, {
+            //  find the pair `controller.action` and execute it
             dispatch: function(stateId, params, options) {
                 // console.log(this.app);
-                // reset cancellation
+                // reset cancel ability
                 this.isExecutionCanceled = false;
                 // @NOTE: param should already be an named object
                 var state = this.getState(stateId);
@@ -291,12 +331,12 @@ define(
                 // could be used to monitor some global stuff (user access, ...)
                 // @TODO allow routing alteration
                 com.publish('dispatcher:beforeDispatch', state, params, this);
+                // if (this.isExecutionCanceled) { return; }
 
                 // dispatch event to allow loader plug-in
                 // load assets
                 // when done dispatch another event
-
-                // this should async
+                // ... this will be async (assets loading)
                 this.execute(command, state, params);
             },
 
@@ -305,12 +345,14 @@ define(
                 return this.states[stateId];
             },
 
-            // should be able to cancel dispatching
+            // should be able to cancel dispatching in some preDispatch method
             cancelExecution: function() {
                 this.isExecutionCanceled = true;
             },
 
-            // execute the controller::action associated to the given route
+            // compare new controller with previous one
+            // destroy last one if differrent
+            // execute the controller::action method
             execute: function(command, state, params) {
                 if (this.isExecutionCanceled) { return; }
 
@@ -359,12 +401,14 @@ define(
                 com.publish('dispatcher:afterDispatch', instance, action, state, params);
             },
 
+
+            // the following is redondant with event system...
+
             // execute all the registered methods for predispatch
             _preDispatch: function() {},
 
             // execute all the registered methods for postdispatch
             _postDispatch: function() {},
-
 
             //  allow to register method for pre|post dispatching
             //  @param type <string> [predispatch|postdispatch] which queue to add the callback
@@ -376,22 +420,22 @@ define(
         /*
          *  AbstractController
          *  -------------------------------------------------------
-         *  the define of the controller should call the usefull views to be sure
-         *  eveything is preloaded and also can be minified properly
-         *  this also allow to have a simple idea of the controller dependencies
-         *  app is locked just before calling he action
+         *  application controllers should be extended from this object
+         *  allows to have higher control of the current state and remove many logic from the view
+         *  finally allowing to use more efficient views
          *
          *  MyController = GG.AbstractController.extend({
          *      // action
          *      index: function(route, params) {
-         *          // has access to :
+         *          // has internal access to :
          *          // this.layout
-         *          // this.currentState (backbone model ?)
+         *          // this.state (backbone model ?)
+         *          // this.services.get('') // to get a global service
          *
          *          // build models according to params
          *          // get views from the factory (use requirejs path as id)
          *
-         *          // use a promise like pattern tobuild the
+         *          // use a promise like pattern to build the
          *          // transition between views in each regions
          *          // the regions could expose a default transition system
          *
@@ -415,6 +459,7 @@ define(
             resume: function() {
                 // publish 'something' to unlock the app
                 // console.log('   => unlock app');
+                com.publish('ui:unlock');
             },
 
             destroy: function() {
@@ -423,10 +468,11 @@ define(
         });
 
         //  @TODO AppController to handle loader (through events), 404 fallbacks, globals behaviors
-        //  loader should also listen to custom events to allow it's control from a controller
+        //  loader should also listen custom events to allow it's fine control from a controller
 
-        // --------------------------------------
+        // ---------------------------------------------
         // dump controller for testing
+        // ---------------------------------------------
         var MyController = AbstractController.extend({
             // common
             initialize: function() {
@@ -463,6 +509,8 @@ define(
         // test that `extend` is well transmitted from AbstractController
         var Control2 = MyController.extend({});
 
+
+        // ---------------------------------------------
         // wait for the DOM
         $('document').ready(function() {
             // intialize the framework
